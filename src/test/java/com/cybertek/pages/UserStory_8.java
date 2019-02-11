6b47fd467376d47c11ca016374ddcb96dd9e5212
Xapreezy <46847346+Xapreezy@users.noreply.github.com>
2019-02-11 00:48:41 -0500

    Update UserStory_8.java

--- src/test/java/com/cybertek/pages/UserStory_8.java
+++ src/test/java/com/cybertek/pages/UserStory_8.java
@@ -2,7 +2,6 @@ package com.cybertek.pages;
 
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -24,5 +23,25 @@ public class UserStory_8 {
     @FindBy(xpath = "/html/body/div[1]/div[1]/div[1]/div[8]/ul[1]/li[2]/ul")
     public WebElement MasterDataOptions;
 
+    @FindBy(xpath = "/html/body/div[1]/div[1]/div[1]/div[8]/ul[1]/li[2]/ul/li[1]/a")
+    public WebElement Customers;
+
+    @FindBy(xpath = "/html/body/div[1]/div[1]/div[1]/div[8]/ul[1]/li[2]/ul/li[2]/a/span")
+    public WebElement Sellable_Products;
+    @FindBy(xpath = "/html/body/div[1]/div[1]/div[1]/div[8]/ul[2]/li[1]/a/span")
+    public WebElement Documents_Under_Purchases;
+    @FindBy(xpath = "/html/body/div[1]/div[1]/div[1]/div[8]/ul[2]/li[2]/a/span")
+    public WebElement Master_Data_Under_purchases;
+    @FindBy(xpath = "/html/body/div[1]/div[1]/div[1]/div[8]/ul[2]/li[1]/ul/li[1]/a/span")
+    public WebElement VonderBills_Under_Documents_Under_Purchases;
+    @FindBy(xpath = "/html/body/div[1]/div[1]/div[1]/div[8]/ul[2]/li[1]/ul/li[2]/a/span")
+    public WebElement VandorCreaditNOtes_Under_Documents_Under_Purchases;
+
+    @FindBy(xpath = "/html/body/div[1]/div[1]/div[1]/div[8]/ul[2]/li[1]/ul/li[3]/a/span")
+    public WebElement Payments_Under_Documents_Under_Purchases;
+    @FindBy(xpath = "/html/body/div[1]/div[1]/div[1]/div[8]/ul[2]/li[2]/ul/li[1]/a")
+    public WebElement Vandor_Under_Master_Data_Under_purchases;
+    @FindBy(xpath = "/html/body/div[1]/div[1]/div[1]/div[8]/ul[2]/li[2]/ul/li[2]/a/span")
+    public WebElement Purchasesable_products_Under_Master_Data_Under_purchases;
 }
 
